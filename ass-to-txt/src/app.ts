400f592026b932b7f3c61d751cde8cc94f27f913
Jun Guo <gjuoun@gmail.com>
2022-07-29 16:44:55 -0300

    done

--- ass-to-txt/src/app.ts
+++ ass-to-txt/src/app.ts
@@ -12,14 +12,16 @@ const headerRegex = /\[Script Info\].*Effect, Text/ms;
 const leftStartRegex = /Dialogue.*,,/g;
 const largeLineBreak = /\\N\{\\fs38\}/g;
 
-filePaths.forEach((path) => {
-  fs.readFile(path, { encoding: "utf16le" }, (err, data) => {
+filePaths.forEach((filePath) => {
+  const pathInfo = path.parse(filePath); //?
+  const newPath = path.join(pathInfo.dir, "../txt", pathInfo.name + ".txt"); //?
+
+  fs.readFile(filePath, { encoding: "utf16le" }, (err, data) => {
     const refinedText = data
       .replace(headerRegex, "")
       .replace(leftStartRegex, "")
       .replace(largeLineBreak, "\n");
 
-    const newPath = path.replace(".ass", ".txt");
     fs.writeFile(newPath, refinedText, { encoding: "utf16le" }, (err) => {
       if (!err) {
         console.log(`${newPath} created`);
